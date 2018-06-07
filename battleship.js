a242de64250651c52111265635a7bfe490c2f11b
Darcy Myronuk <darcy.myronuk@gmail.com>
2018-06-07 22:23:36 +0000

    Add constructors for Game Ship and Player objects

--- battleship.js
+++ battleship.js
@@ -3,6 +3,52 @@ let options = {
   boardHeight: 500
 }
 
+class Ship {
+  constructor(name, length) {
+    this.name = name;
+    this.length = length;
+    this.hits = [];
+    for(let i = 0; i < length; i++){
+      hits.push(0);
+    }
+  }
+};
+
+class Player {
+  constructor(id){
+    this.id = id;
+  }
+};
+
+class Game {
+  constructor(){
+    this.currentPlayerId = 1;
+    this.board = this.initBoard();
+    this.players = this.initPlayers();
+  }
+
+  initBoard(){
+    let outArr = [];
+    for(let i = 0; i < 10; i++){
+      let row = [];
+      for(let j = 0; j < 10; j++){
+        row.push([])
+      }
+      outArr.push(row);
+    }
+    return outArr;
+  }
+
+  initPlayers(){
+    let players = []
+    for(let i = 1; i < 3; i++){
+      let curPlayer = new Player(i);
+      players.push(curPlayer);
+    }
+    return players;
+  }
+};
+
 let createSquare = (parentRow, xVal, yVal) => {
   let square = $("<div/>").addClass("board-square");
   let squareWidth = options.boardWidth / 10;
@@ -29,5 +75,6 @@ let createBoardRow = (boardWidth, boardHeight) => {
   }
 };
 
-
 createBoardRow(options.boardWidth, options.boardHeight);
+
+var curGame = new Game()
